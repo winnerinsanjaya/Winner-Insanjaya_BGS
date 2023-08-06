ba3167bff8e01a6165a2372cbd810e5671cd367e
winnerinsanjaya <61474469+winnerinsanjaya@users.noreply.github.com>
2023-08-07 05:00:37 +0700

    Player Acc Struct

--- Assets/Scripts/Global/DataStruct/PlayerAccStruct.cs
+++ Assets/Scripts/Global/DataStruct/PlayerAccStruct.cs
@@ -1,18 +1,84 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
-public class PlayerAccStruct : MonoBehaviour
+
+[Serializable]
+public class PlayerAccStruct
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Face face;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public Head head;
+
+    public HeadAcc headAcc;
+
+    public Top top;
+
+    public Gloves gloves;
+
+    public Bottom bottom;
+
+    public Boots boots;
+
+    public Weapon weapon;
 }
+
+[Serializable]
+public class Face
+{
+    public SpriteRenderer face;
+}
+
+[Serializable]
+public class Head
+{
+    public SpriteRenderer head;
+}
+
+[Serializable]
+public class HeadAcc
+{
+    public SpriteRenderer headAcc;
+}
+
+[Serializable]
+public class Top
+{
+    public SpriteRenderer bodyUp;
+    public SpriteRenderer shoulderLeft;
+    public SpriteRenderer shoulderRight;
+}
+
+[Serializable]
+public class Bottom
+{
+
+    public SpriteRenderer pelvis;
+    public SpriteRenderer legLeft;
+    public SpriteRenderer legRight;
+}
+
+[Serializable]
+public class Boots
+{
+    public SpriteRenderer bootsLeft;
+    public SpriteRenderer bootsRight;
+}
+
+[Serializable]
+public class Gloves
+{
+    public SpriteRenderer leftWrist;
+    public SpriteRenderer rightWrist;
+    public SpriteRenderer leftElbow;
+    public SpriteRenderer rightElbow;
+}
+
+[Serializable]
+public class Weapon
+{
+    public SpriteRenderer weapon;
+}
+
+
