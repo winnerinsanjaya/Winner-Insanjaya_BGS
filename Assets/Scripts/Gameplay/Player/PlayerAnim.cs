1e3227b3d8430d6352afd7fcbd4f033a5142d9a7
winnerinsanjaya <61474469+winnerinsanjaya@users.noreply.github.com>
2023-08-07 04:28:40 +0700

    Shop Environment

--- Assets/Scripts/Gameplay/Player/PlayerAnim.cs
+++ Assets/Scripts/Gameplay/Player/PlayerAnim.cs
@@ -3,42 +3,46 @@ using System.Collections.Generic;
 using UnityEngine;
 using BGS.scriptable;
 
-public class PlayerAnim : MonoBehaviour
+
+namespace BGS.Player
 {
-    public AnimList[] animList;
-
-    private int selectAnim;
-
-    public Animator animator;
-
-    public int animationLayer;
-
-    public List<string> states = new List<string>();
-
-    private void Start()
+    public class PlayerAnim : MonoBehaviour
     {
-        foreach (AnimList al in animList)
+        public AnimList[] animList;
+
+        private int selectAnim;
+
+        public Animator animator;
+
+        public int animationLayer;
+
+        public List<string> states = new List<string>();
+
+        private void Start()
         {
-            for (int i = 0; i < al.stateName.Count; i++)
+            foreach (AnimList al in animList)
             {
-                string s = al.stateName[i];
-                states.Add(s);
+                for (int i = 0; i < al.stateName.Count; i++)
+                {
+                    string s = al.stateName[i];
+                    states.Add(s);
+                }
             }
+            animator = animator.GetComponent<Animator>();
+            SelectAnimation(selectAnim);
         }
-        animator = animator.GetComponent<Animator>();
-        SelectAnimation(selectAnim);
-    }
 
-    private void OnEnable()
-    {
-        SelectAnimation(selectAnim);
-    }
+        private void OnEnable()
+        {
+            SelectAnimation(selectAnim);
+        }
 
-    public void SelectAnimation(int index)
-    {
-        if (index < 0 || index >= states.Count || animator == null)
-            return;
-        selectAnim = index;
-        animator.Play(states[index], animationLayer);
+        public void SelectAnimation(int index)
+        {
+            if (index < 0 || index >= states.Count || animator == null)
+                return;
+            selectAnim = index;
+            animator.Play(states[index], animationLayer);
+        }
     }
 }
